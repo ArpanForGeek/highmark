0e02f5bf50ae9bbfa1f151b61ed5d327e9d8d2fe
Bandyopadhyay <a.l.bandyopadhyay@accenture.com>
2021-06-21 18:40:59 +0530

    added queries

---
+++ src/main/resources/member_dml.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.member(
+activecoverageind, addressline1, addressline2, addressline3, addressline4, addresstypecode, addresstypecodedescription, city,
+countryname, dateofbirth, effectivedate, enddate, extension, firstname, group_number, group_number_icis, group_phone_number,
+lastname, memberid, middlename, nameprefix, namesuffix, phonenumber, phonetype, sourcesystem, state,
+ taxid, zipcode, umi, member_relationship_to_policy_holder, member_type, plan_indicator)
+VALUES ('Y', '0591 Gonzalez Street', 'Apartment 13','','','G','General Delivery Address','South Collinburgh','US',
+'19550803','20190416','20211007','558','Stuti','990211111','1242543676','5551117777','Key','7179763703','',
+'Mrs.','PhD','9617063260','Landline','HighMark','Massachusetts','4210791','69482','4085624876987','children','Premium','M');
